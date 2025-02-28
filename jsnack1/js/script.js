5f877969b2710ec92429245e7ec086cce5c16ed0
DanielScardigno <danielscardigno2@gmail.com>
2025-02-28 12:29:28 +0100

    finished third exercise + imiproved first exercise code

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,45 +1,14 @@
 const auto = [
-    {
-        marca: "Toyota",
-        modello: "Supra mk4",
-        alimentazione: "Benzina",
-    }, {
-        marca: "FIAT",
-        modello: "Panda 4x4",
-        alimentazione: "Diesel",
-    }, {
-        marca: "Porsche",
-        modello: "Panamera 4s",
-        alimentazione: "Diesel",
-    }, {
-        marca: "Ferrari",
-        modello: "296 GTB",
-        alimentazione: "Benzina",
-    }, {
-        marca: "Porsche",
-        modello: "Tycan Turbo S",
-        alimentazione: "Elettrico",
-    }, {
-        marca: "Chevrolet",
-        modello: "Camaro",
-        alimentazione: "Benzina",
-    }, {
-        marca: "Audi",
-        modello: "A3 g-tron",
-        alimentazione: "Metano",
-    }, {
-        marca: "Dodge",
-        modello: "Charger Daytona",
-        alimentazione: "Elettrico",
-    }, {
-        marca: "Opel",
-        modello: "Corsa",
-        alimentazione: "GPL",
-    }, {
-        marca: "FIAT",
-        modello: "500 GPL",
-        alimentazione: "GPL",
-    }
+    {marca: "Toyota", modello: "Supra mk4", alimentazione: "Benzina"},
+    {marca: "FIAT", modello: "Panda 4x4", alimentazione: "Diesel"},
+    {marca: "Porsche", modello: "Panamera 4s", alimentazione: "Diesel"},
+    {marca: "Ferrari", modello: "296 GTB", alimentazione: "Benzina"},
+    {marca: "Porsche", modello: "Tycan Turbo S", alimentazione: "Elettrico"},
+    {marca: "Chevrolet", modello: "Camaro", alimentazione: "Benzina"},
+    {marca: "Audi", modello: "A3 g-tron", alimentazione: "Metano"},
+    {marca: "Dodge", modello: "Charger Daytona", alimentazione: "Elettrico"}, 
+    {marca: "Opel", modello: "Corsa", alimentazione: "GPL"},
+    {marca: "FIAT", modello: "500 GPL", alimentazione: "GPL"}
 ]
 
 const autoBenzina = []
---
+++ jsnack3/js/script.js
@@ -0,0 +1,16 @@
+const animali = [
+    {nome: "Lupo", famiglia: "Canidi", classe: "Mammiferi"},
+    {nome: "Volpe", famiglia: "Canidi", classe: "Mammiferi"},
+    {nome: "Blue-jay", famiglia: "Corvidi", classe: "Uccelli"}
+]
+
+const mammiferi = []
+
+for (let i = 0; i < animali.length; i++) {
+    let animaleCorrente = animali[i];
+    let classeAnimale = animaleCorrente.classe;
+    if (classeAnimale === "Mammiferi") {
+        mammiferi.push(animaleCorrente);
+    }
+}
+console.log(mammiferi);
